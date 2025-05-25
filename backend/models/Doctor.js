e61e14924cdbb92b69f7c15718f0c76656956029
Mohammad-Sami8552 <mohammadsami8552@gmail.com>
2025-05-25 12:17:14 +0530

    Full Stack Web uploaded

---
+++ backend/models/Doctor.js
@@ -0,0 +1,9 @@
+// // models/Doctor.js
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const doctorSchema=new Schema({
+name:{type:String, required: true},
+speciality: {type:String, required: true}
+});
+const Doctor=mongoose.model('Doctor',doctorSchema);
+module.exports= Doctor;
